c2f28416a5efd272507f8b07baa78082c61202bc
Shunyo Ogawa <shunyo44@gmail.com>
2022-04-05 11:05:29 +0900

    add add_documentation method

--- mms_python_adapter/mms_python_adapter.py
+++ mms_python_adapter/mms_python_adapter.py
@@ -5,7 +5,7 @@ from mms_python_client.models import Element, ElementsRequest, ElementsResponse
 
 class MMSAdapter(object):
 
-    def __init__(self, server, project_id, ref_id):
+    def __init__(self, server: str, project_id: str, ref_id: str) -> None:
         self.config = Configuration()
         self.config.host = server
 
@@ -16,7 +16,7 @@ class MMSAdapter(object):
         self.project_id = project_id
         self.ref_id = ref_id
 
-    def login(self, username, password):
+    def login(self, username: str, password: str) -> str:
         self.config.username = username
         self.config.password = password
         self.client = ApiClient(self.config)
@@ -34,7 +34,7 @@ class MMSAdapter(object):
     def get_element(self, element_id: str) -> Element:
         return self.elem_api.get_element(self.project_id, self.ref_id, element_id)._data_store['elements'][0]
 
-    def update_element_value(self, element_id, value) -> ElementsResponse:
+    def update_element_value(self, element_id: str, value: str) -> ElementsResponse:
         element = self.get_element(element_id)
         default_value: dict = element['defaultValue']
         default_value['value'] = value
@@ -52,6 +52,18 @@ class MMSAdapter(object):
         element_respo = self.elem_api.create_or_update_elements(self.project_id, self.ref_id,
                                                                 element_req)
 
+        return element_respo
+
+    def get_documentation(self, element_id: str) -> str:
+        element = self.get_element(element_id)
+        return element['documentation']
+
+    def update_element_documentation(self, element_id: str, documentation: str) -> ElementsResponse:
+        element = self.get_element(element_id)
+        element.set_attribute('documentation', documentation)
+        element_req = ElementsRequest([element])
+        element_respo = self.elem_api.create_or_update_elements(self.project_id, self.ref_id,
+                                                                element_req)
         return element_respo
 
 
